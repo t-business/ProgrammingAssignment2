df2af249a147d642b2779e8a8a3260ede5e0332d
t_business <t_business@verizon.net>
2015-03-22 19:24:21 -0400

    Programming assignment 2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix will cache the mean of the inversion of a square matrix
+## so means of inversions do not have to be recalculated in the future 
 
-## Write a short comment describing this function
+## makeCachMatrix calls cacheSolve to get the inverse and stores in a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        checklist <<- list("matrix" = x)
+        ix <<- cacheSolve()
+        invlist <<- list("matrix" = x, "inverse"= ix)
+        return(invlist)
 }
 
+## cacheSolve  calculates the inverse of invertible square matrix
+## UNLESS inverse already calculated
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function() {
+        ## Check to see if invlist exists. If not, create one.
+        if(!exists("invlist")){
+                invlist <<- list("matrix" = matrix(0), "inverse" = matrix(0))
+        }
+        ## Return a matrix that is the inverse of 'x' if already calculated
+        if(identical(checklist$matrix, invlist$matrix)){
+                message("getting cached data")
+                return(invlist$inverse)
+        }
+        
+        ## Since not already calculated, return calculated inverse of 'x'
+        else {
+                message("calculating inverse")
+                ix <- solve(checklist$matrix)
+                return(ix)
+        }
 }
